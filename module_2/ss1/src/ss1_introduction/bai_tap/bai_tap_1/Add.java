a5b9c8dc123864b61cd6bfc3fa39654b1389ab56
minhtien100396 <minhtien100396@gmail.com>
2022-07-29 16:47:31 +0700

    g

---
+++ module_2/ss1/src/ss1_introduction/bai_tap/bai_tap_1/Add.java
@@ -0,0 +1,10 @@
+package ss1_introduction.bai_tap.bai_tap_1;
+
+
+
+import java.util.Scanner;
+
+public class Add {
+    Scanner scanner= new Scanner(System.in);
+
+}
---
+++ module_2/ss1/src/ss1_introduction/bai_tap/bai_tap_1/StudentClass.java
@@ -0,0 +1,4 @@
+package ss1_introduction.bai_tap.bai_tap_1;
+
+public class StudentClass {
+}
